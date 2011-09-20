85510296e30b2e2b0b3ee33ae8bed7f3660ef98b
Dharshun Sridharan <dharshun.sridharan@uqconnect.edu.au>
2011-09-20 11:52:42 +1000

    Initial Files

---
+++ ISOConfig/src/isoconfig/ConfigReader.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isoconfig;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author G73
+ */
+public class ConfigReader {
+    /*public ConfigReader(){
+        /*int lines = checkNumberOfLines(inputFile);
+        String[] fileArray = parseToArray(inputFile,lines);*/
+        /* PASS FILE ARRAY TO LINE CHECKER */
+    /*}*/
+    public static int  checkNumberOfLines(String inputFile){
+        int lines = 0;
+        try {
+            // Open the file that is the first 
+            // command line parameter
+            FileInputStream fstream = new FileInputStream(inputFile);
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            //Read File Line By Line
+            while ((strLine = br.readLine()) != null) {
+                // Print the content on the console
+                lines++;
+            }
+            //Close the input stream
+            in.close();
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+    return lines ;
+    }
+    public static String[] parseToArray(String inputFile, int lines){
+        String[] fileArray = new String[lines];
+        int i = 0;
+        try {
+            // Open the file that is the first 
+            // command line parameter
+            FileInputStream fstream = new FileInputStream(inputFile);
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            //Read File Line By Line
+            while ((strLine = br.readLine()) != null) {
+                fileArray[i]=strLine;
+                i++;
+            }
+            //Close the input stream
+            in.close();
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+        return fileArray;
+    }
+    
+}
---
+++ ISOConfig/src/isoconfig/LineChecker.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isoconfig;
+
+/**
+ *
+ * @author G73
+ */
+public class LineChecker {
+    public LineChecker(String[] fileArray) {
+        /* ANALYSE */
+    } 
+}
