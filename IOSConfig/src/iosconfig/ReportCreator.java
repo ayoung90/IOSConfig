bf02511cbeba1edf9352106c0167613567060c3e
Adam Young <adam_sez_hi@hotmail.com>
2011-09-20 18:21:56 +1000

    Renamed project + package from isoconfig to iosconfig due to boredom ;p

--- IOSConfig/src/iosconfig/ReportCreator.java
+++ IOSConfig/src/iosconfig/ReportCreator.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package isoconfig;
+package iosconfig;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
