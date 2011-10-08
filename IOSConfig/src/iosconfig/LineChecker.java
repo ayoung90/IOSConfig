b3924574c9c30203f607e86529d1a953268fa30b
unknown <G73@.(none)>
2011-10-08 13:34:39 +1000

    Bathurst Update

--- IOSConfig/src/iosconfig/LineChecker.java
+++ IOSConfig/src/iosconfig/LineChecker.java
@@ -12,9 +12,44 @@ import java.util.LinkedList;
  */
 public class LineChecker {
 
-    public LinkedList<Vulnerability> VulnList = new LinkedList<Vulnerability>();
+    public static LinkedList<Vulnerability> VulnList = new LinkedList<Vulnerability>();
 
-    public LineChecker(String[] fileArray) {
+    public static LinkedList<Vulnerability> checkLines(String[] fileArray) {
+        IOSConfig ios = new IOSConfig();
+        for (int i = 0; i < fileArray.length; i++) {
+            if (!fileArray[i].contains("!")) {
+                /* Password Checker */
+                if (fileArray[i].startsWith("enable")) {
+                    if (!fileArray[i].contains("secret")) {
+                        VulnList.add(new Vulnerability(i + 1, fileArray[i], "A"));
+                    }
+                    if (fileArray[i].contains(" 7 ") || fileArray[i].contains(" 0 ")) {
+                        VulnList.add(new Vulnerability(i + 1, fileArray[i], "B"));
+                    }
+                }
+                if (fileArray[i].contains("password")) {
+                    if (fileArray[i].contains(" 7 ") || fileArray[i].contains(" 0 ")) {
+                        VulnList.add(new Vulnerability(i + 1, fileArray[i], "B"));
+                    }
+                }
+                if (fileArray[i].startsWith("access-list")) {
+                    if ((fileArray[i].contains(" 10.")) || (fileArray[i].contains(" 192.168")) || (fileArray[i].contains(" 172."))) {
+                        if (fileArray[i].contains("any")) {
+                            if (!fileArray[i].contains("deny")) {
+                                VulnList.add(new Vulnerability(i + 1, fileArray[i], "C"));
+                            }
+                        }
+                    }
+                }
+                if (!fileArray[i].contains("deny")) {
+                    if (fileArray[i].contains("any any")) {
+                        VulnList.add(new Vulnerability(i + 1, fileArray[i], "C"));
+                    }
+                }
+            }
+        }
         /* ANALYSE */
+
+        return VulnList;
     }
 }
